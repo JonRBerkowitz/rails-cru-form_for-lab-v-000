16fcecf83c7fce528cceed3fee239d311dd76c30
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-04-09 22:28:28 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,15 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
   end
 
+  def new
+    @artist = Artist.new()
+  end
+
   def create
+    @artist = Artist.new(params)
+    @artist.save
+
+    redirect_to artist_path(@artist)
   end
 
   def update
