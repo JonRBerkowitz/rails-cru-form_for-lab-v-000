66c26221c7624df6de46781e784e2a92e36ba4b1
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-04-09 22:52:13 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,13 +1,38 @@
 class GenresController < ApplicationController
+  def index
+    @genre = Genre.all
+  end
 
-    def create
-    end
+  def new
+    @genre = Genre.new
+  end
 
-    def update
-    end
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
 
-    def show
-      @genre = Genre.find_by(params[:id])
-    end
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find_by(params[:id])
+  end
+
+  def update
+    @genre = Genre.find_by(params[:id])
+    @genre.update(genre_params(:name, :bio))
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+    @genre = Genre.find_by(params[:id])
+  end
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+
 
 end
